e69df316194f9b6ad748e18c516e6e318e932669
Kayennne <s10242715@connect.np.edu.sg>
2023-02-05 12:20:23 +0800

    menu page done but not connected - kayen

--- script.js
+++ script.js
@@ -30,6 +30,25 @@ function showSlides() {
 
 
 
+// ========================================= MENU =========================================
+
+const navBar = document.querySelector("nav"),
+  menuBtns = document.querySelectorAll(".menu-icon"),
+  overlay = document.querySelector(".overlay");
+
+menuBtns.forEach((menuBtn) => {
+  menuBtn.addEventListener("click", () => {
+    navBar.classList.toggle("open");
+  });
+});
+
+overlay.addEventListener("click", () => {
+  navBar.classList.remove("open");
+});
+
+
+
+
 
 
 //login
