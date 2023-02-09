948c44bc592c79fe7fa90c105d5ee2ac7309327d
Kayennne <s10242715@connect.np.edu.sg>
2023-02-09 11:38:56 +0800

    update to index, completed mental wellness - kayen api update - jiayun
    Co-authored-by: JiaYun5 <JiaYun5@users.noreply.github.com>

--- script.js
+++ script.js
@@ -53,34 +53,85 @@ function showSlides() {
 
 
 //login
+$(document).ready(function () { 
+  const APIKEY = "63dfedfc3bc6b255ed0c46c4";
 
-  //retrieve form information when submit
-  let myNameEl = document.getElementById("myName");
-  let username = localStorage.getItem("username");
-  if (username != null && username != "") {
-    myNameEl.innerHTML = username;
-  }
-        
-   document.addEventListener("submit", function (event) {
-    //prevent default action of the form from actually submitting
-     event.preventDefault();
-     
-     //retrieve form values
-     let myName = document.getElementById("uname").value;
-     console.log(myName);
-     //set the name
-     localStorage.setItem('username',myName);  
-    document.getElementById("myName").innerHTML = myName;
-     //document.getElementById('myName').textContent = myName;
-  
-     //clear the form upon each sumission
-    document.getElementById("myFrm").reset();
-   });
-      
+  $("#loginbtn").on("click", function (e) {
+    e.preventDefault();
+
+    let loginUsername = $("#login_username").val();
+    let loginPassword = $("#login_password").val();
+
+    let contact = {
+      "username": loginUsername,
+      "password": loginPassword,
+    };
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://reach-01ac.restdb.io/rest/contacts",
+      "method": "GET",
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": APIKEY,
+        "cache-control": "no-cache"
+      }
+    }
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+  })
+})
+
+
+
+
+
+
 //register
+$(document).ready(function () { 
+  const APIKEY = "63dfedfc3bc6b255ed0c46c4";
 
+  $("#registerbtn").on("click", function (e) {
+    e.preventDefault();
 
+    let contactUsername = $("#create_username").val();
+    let contactEmail = $("#create_email").val();
+    let contactPassword = $("#create_password").val();
+    let contactCfmPassword = $("#password_cfm").val();
 
+    var jsondata = {
+      "username": contactUsername,
+      "email": contactEmail,
+      "password": contactPassword
+    };
+
+    if (contactPassword == contactCfmPassword){
+      console.log("Password Matches")
+
+      var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://reach-01ac.restdb.io/rest/contacts",
+        "method": "POST",
+        "headers": {
+          "content-type": "application/json",
+          "x-apikey": APIKEY,
+          "cache-control": "no-cache"
+        },
+        "processData": false,
+        "data": JSON.stringify(jsondata)
+      }
+    }
+    
+    
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+  })
+})
 
 
 
